6fc75141c0434acbb1da5478034fbbd2522e0604
thoamsy <c6swift@gmail.com>
2021-06-17 00:02:51 +0800

    Initial Commit

---
+++ Shared/GradientGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientGridApp.swift
+//  Shared
+//
+//  Created by yk on 2021/6/17.
+//
+
+import SwiftUI
+
+@main
+struct GradientGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
